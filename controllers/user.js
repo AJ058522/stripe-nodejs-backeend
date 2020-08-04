d85bf6dd1d2396e2dcfb81bdadfd444a0aa45511
Geovane <geovaneartedesign@gmail.com>
2020-08-04 16:23:35 -0400

    m

--- controllers/user.js
+++ controllers/user.js
@@ -230,8 +230,8 @@ function registerUser(req, res) {
       // create reusable transporter object using the default SMTP transport
       let transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
-        port: 587,
-        secure: false, // true for 465, false for other ports
+        port: 465,
+        secure: true, // true for 465, false for other ports
         auth: {
           user: process.env.EMAIL, // generated ethereal user
           pass: process.env.PASSWORD,
@@ -239,9 +239,10 @@ function registerUser(req, res) {
         },
         
     
-         tls:{
+        tls: {
+          // do not fail on invalid certs
           rejectUnauthorized: false
-         }
+      }
       });
     
       
