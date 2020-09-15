4ae34e70f693204ba7cd41ab2bb6fcc23613b056
Home <home@Homes-Mac-mini.attlocal.net>
2020-09-15 18:07:08 -0400

    m

--- config/config.js
+++ config/config.js
@@ -19,7 +19,7 @@ if(process.env.NODE_ENV === 'dev'){
 }else{
    
 
-    urlDB = process.env.MONGO_TESTE;
+    urlDB = process.env.MONGO_URI;
      //urlDB  //mongodb+srv://developer-dci-pr:Geov2020!@cluster0-nxlwx.mongodb.net/DC_cond_DB
     // urlDB = 'mongodb+srv://geovanedasilva:nelliebeach4205@cluster0-bipz8.mongodb.net/tests'
 
